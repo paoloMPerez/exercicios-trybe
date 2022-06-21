41993e3470a7fa03f7dfa59d95647be62c91639a
Paolo Perez <paolomonteiro@gmail.com>
2022-06-21 19:40:14 -0300

    exercicios 1 ao 4 concluidos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/agora-a-pratica/exercicio-2.js
@@ -0,0 +1,8 @@
+const a = 50;
+const b = 32;
+
+if (a > b) {
+  console.log("'a' é maior que 'b'");
+} else {
+  console.log("'b' é maior que 'a'");
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/agora-a-pratica/exercicio-3.js
@@ -0,0 +1,11 @@
+const a = 3;
+const b = 8;
+const c = 10;
+
+if (a > b && a > c) {
+  console.log("O maior número é: " + a + " (a)");
+} else if (b > a && b > c) {
+  console.log("O maior número é: " + b + " (b)");
+} else {
+  console.log("O maior número é: " + c + " (c)");
+}
