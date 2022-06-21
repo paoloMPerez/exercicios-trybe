d7366eeb0937ee45fe3dcea28179e3593aaf33d7
Paolo Perez <paolomonteiro@gmail.com>
2022-06-21 16:14:18 -0300

    exercicio corrigido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/variaveis/variaveis.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/variaveis/variaveis.js
@@ -1,13 +1,13 @@
-const myName = Paolo
-const birthCity = "Volta Redonda - RJ"
-let birthYear = 1977
+const myName = Paolo;
+const birthCity = "Volta Redonda - RJ";
+let birthYear = 1977;
 
-console.log(myName)
-console.log(birthCity)
-console.log(birthYear)
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
 
-birthYear = 2030
-console.log(birthYear)
+birthYear = 2030;
+console.log(birthYear);
 
-birthCity = "Rio de Janeiro"
-console.log(birthCity)
+birthCity = "Rio de Janeiro";
+console.log(birthCity);
