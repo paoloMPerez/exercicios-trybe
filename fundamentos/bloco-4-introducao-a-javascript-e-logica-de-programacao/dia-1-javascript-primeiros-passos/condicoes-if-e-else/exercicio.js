b9918f2fb69e4e07ec528c925a172b73ae5a3d69
Paolo Perez <paolomonteiro@gmail.com>
2022-06-21 18:56:45 -0300

    correcao do exercicio de condicoes if e else concluido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/condicoes-if-e-else/exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/condicoes-if-e-else/exercicio.js
@@ -1,9 +1,11 @@
 const nota = 80;
 
-if (grade >= 80) {
+if (nota >= 80) {
   console.log("Parabéns, você foi aprovada(o)!");
 } else if (grade < 80 && grade >= 60) {
   console.log("Você está na nossa lista de espera");
 } else if (grade < 60) {
   console.log("Você foi reprovada(o)");
 }
+
+console.log(nota);
