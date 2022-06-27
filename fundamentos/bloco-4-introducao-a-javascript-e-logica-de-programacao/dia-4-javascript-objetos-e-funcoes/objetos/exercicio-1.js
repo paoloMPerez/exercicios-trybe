113786257a8c355d10ebfdf7f249f6e21abe593c
Paolo Perez <paolomonteiro@gmail.com>
2022-06-27 19:49:39 -0300

    exercicio 1 concluido

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos/exercicio-1.js
@@ -0,0 +1,18 @@
+// Crie um objeto player contendo as variáveis listadas abaixo:
+
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
+
+// SOLUÇÃO:
+
+let player = {
+  name: "Marta",
+  lastName: "Silva",
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3,
+  },
+};
