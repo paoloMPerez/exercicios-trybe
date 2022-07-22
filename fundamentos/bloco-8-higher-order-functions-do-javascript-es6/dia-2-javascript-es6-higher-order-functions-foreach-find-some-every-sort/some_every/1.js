e351ecb85799bce4e10bd560def4ec0c4191275d
Paolo Perez <paolomonteiro@gmail.com>
2022-07-22 18:46:27 -0300

    exercicio 1 de some/every concluido

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/some_every/1.js
@@ -0,0 +1,9 @@
+// Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false, use some;
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  return arr.some((containName) => containName === name);
+}
+
+console.log(hasName(names, 'Ana'))
