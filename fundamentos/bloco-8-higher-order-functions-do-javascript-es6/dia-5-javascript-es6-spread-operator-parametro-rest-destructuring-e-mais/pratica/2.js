7c8e91dd7336c62ebc59fd79c29973c372184736
Paolo Perez <paolomonteiro@gmail.com>
2022-07-28 08:11:20 -0300

    Exercicio concluido

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/pratica/2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/pratica/2.js
@@ -1,5 +1,6 @@
 // Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
 // Dica: use parâmetro rest.
 
-
 const sum = (...numbers) => numbers.reduce(((total, number) => total + number), 0);
+
+console.log(sum(8, 5));
